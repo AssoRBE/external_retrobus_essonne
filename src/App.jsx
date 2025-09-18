2a755e14ec3a3aedb694657eca2d746bc05fa41c
AssoRBE <association.rbe@gmail.com>
2025-09-19 01:07:24 +0200

    Mise à jour complète - écrasement forcé

--- src/App.jsx
+++ src/App.jsx
@@ -13,24 +13,20 @@ export default function App() {
       <main
         className="page-body page-with-mark"
         style={{
-          // réglages rapides (tu peux changer à la volée)
           "--page-mark": `url(${pageBg})`,
-          "--mark-pos-x": "left",   // left | center | right | 40px
-          "--mark-pos-y": "center", // top | center | bottom | 20%
-          "--mark-size": "620px",   // 520px, 40vw, contain...
-          "--mark-opacity": "0.08", // 0.05–0.12 selon ton goût
-          "--mark-offset-x": "0px", // décalage fin horizontal (+/-)
-          "--mark-offset-y": "0px", // décalage fin vertical (+/-)
+          "--mark-pos-x": "left",
+          "--mark-pos-y": "center",
+          "--mark-size": "620px",
+          "--mark-opacity": "0.08",
+          "--mark-offset-x": "0px",
+          "--mark-offset-y": "0px",
         }}
       >
-        <h1>Bienvenue sur le site internet de l'Association RétroBus Essonne</h1>
-        <p>Un peu de patience, le site internet arrive !</p>
-        {/* ... tes sections/cartes ... */}
+        <Routes>
+          <Route path="/vehicles" element={<Vehicles />} />
+          <Route path="/vehicles/:id" element={<VehicleDetails />} />
+        </Routes>
       </main>
-      <Routes>
-        <Route path="/vehicles" element={<Vehicles />} />
-        <Route path="/vehicles/:id" element={<VehicleDetails />} />
-      </Routes>
     </Router>
   );
 }
