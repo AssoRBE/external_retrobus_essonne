186b946d1893f7464bcc91285f07b4e7672a958a
AssoRBE <association.rbe@gmail.com>
2025-09-18 21:47:06 +0200

    Mise à jour complète - écrasement forcé

--- src/pages/RetroMerch.jsx
+++ src/pages/RetroMerch.jsx
@@ -1,21 +1,29 @@
-﻿import { Container, Center, Image, Heading, Text, VStack } from "@chakra-ui/react";
+﻿import { Box, Container, Heading, Image, Text } from "@chakra-ui/react";
+import logoMerch from "../assets/retromerch_branding.svg";
 
 export default function RetroMerch() {
   return (
-    <Container maxW="7xl">
-      <VStack spacing={4} mt={2}>
-        <Heading>RétroMerch</Heading>
-        <Text>La boutique arrive bientôt !</Text>
-        <Center w="100%" py={6}>
+    <Box className="page-with-mark">
+      <Container maxW="5xl" py={12} textAlign="center">
+        {/* Titre + logo */}
+        <Heading as="h1" size="2xl" mb={6}>
+          Bienvenue sur le{" "}
           <Image
-            src="/retromerch_coming.png"
-            alt="RétroMerch - Bientôt disponible"
-            maxH="360px"
-            objectFit="contain"
-            fallbackSrc="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="
-          />
-        </Center>
-      </VStack>
-    </Container>
+            src={logoMerch}
+            alt="Logo RétroMerch"
+            display="inline-block"
+            h="60px"
+            mx={2}
+            verticalAlign="middle"
+          />{" "}
+          !
+        </Heading>
+
+        {/* Placeholder pour le futur contenu */}
+        <Text fontSize="lg" color="blackAlpha.700">
+          Retrouvez bientôt nos articles, miniatures et objets autour du patrimoine autobus.
+        </Text>
+      </Container>
+    </Box>
   );
 }
